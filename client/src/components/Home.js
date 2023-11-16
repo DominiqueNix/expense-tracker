03a5c802f38b55ed2ba9c142829c8d226375c02e
DominiqueNix <dominiquenix@google.com>
2023-11-16 15:51:58 -0600

    created react-app

---
+++ client/src/components/Home.js
@@ -0,0 +1,8 @@
+export const Home = () => {
+    return(
+        <div className="main-page">
+            <h1 className="text-center mt-5 display-2">WELCOME TO INEXSPENSIVE</h1>
+            <p className="text-center display-6">YOUR GO TO EXPENSE TRACKING RESOURCE</p>
+        </div>
+    )
+} 
