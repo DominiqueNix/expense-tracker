5ae9e4d7187df4174640da053f14d557238148db
DominiqueNix <dominiquenix@google.com>
2023-11-16 11:43:41 -0600

    added routes/service for income crud operations. updated controllers for expenses

--- src/main/java/com/expenses/expenses/interfaces/IncomeInt.java
+++ src/main/java/com/expenses/expenses/interfaces/IncomeInt.java
@@ -1,11 +1,18 @@
 package com.expenses.expenses.interfaces;
 
-public class IncomeInt {
-    //get all income
+import com.expenses.expenses.models.Income;
 
-    //add income
+import java.util.ArrayList;
 
-    //update income
-
-    //delete income
+public interface IncomeInt {
+    //get all expenses for a user and return array list of their expenses
+    public ArrayList<Income> getAllIncome(long userId);
+    //get one expense return expense model
+    public Income getOneIncome(long id);
+    //add expense to a user void
+    public void addIncome(Income income);
+    //update expense void
+    public void updateIncome(Income income);
+    //delete expense void
+    public void deleteIncome(long id);
 }
